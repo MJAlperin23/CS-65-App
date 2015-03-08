228a1b1cebf694e9eb3dabb81e182d5b26514467
tcathcart <taylor@sighten.io>
2015-03-08 17:27:34 -0400

    added messaging with compile dependencies

---
+++ MyParkinsons/app/src/main/java/edu/dartmouth/cs/myparkinsons/HistoryUploader.java
@@ -0,0 +1,91 @@
+package edu.dartmouth.cs.myparkinsons;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// TODO: update for ExerciseItem object instead of MyRuns
+
+/**
+ * Created by tdc on 2/24/15.
+ */
+public class HistoryUploader {
+
+    public static final String FIELD_ID = "id_col";
+    public static final String FIELD_DATE = "day_of_month";
+    public static final String FIELD_MONTH = "month_of_year";
+    public static final String FIELD_YEAR = "year_col";
+    public static final String FIELD_EXERCISE_TIME = "exercise_time";
+    public static final String FIELD_SPEECH_ATTEMPTS = "speech_attempts";
+    public static final String FIELD_SPEECH_CORRECT = "speech_correct";
+
+    public static void updateHistory(Context context, List<ExerciseItem> entryList, String regId) {
+        try {
+            JSONArray jsonArray = listToJsonArray(entryList);
+            Map<String, String> map = new HashMap<>();
+            map.put("entry_list", jsonArray.toString());
+            map.put("regId", regId);
+            String endpoint = context.getString(R.string.server_addr) + "post.do";
+            String result = ServerUtilities.post(endpoint, map);
+            Log.d("HistoryUploader", result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static JSONArray listToJsonArray(List<ExerciseItem> entryList) throws JSONException {
+
+        JSONArray jsonArray = new JSONArray();
+
+        int len = entryList.size();
+        for (int i = 0; i < len; i++) {
+            ExerciseItem entry = entryList.get(i);
+
+            JSONObject j = new JSONObject();
+            j.put(FIELD_ID, entry.getId());
+            j.put(FIELD_DATE, entry.getDayOfMonth());
+            j.put(FIELD_MONTH, entry.getMonthOfYear());
+            j.put(FIELD_YEAR, entry.getYear());
+
+
+//            JSONArray locationJsonArray = new JSONArray();
+//            Iterable<LatLng> ll = entry.getLocationList();
+//            if (ll != null) {
+//
+//                Iterator<LatLng> iter = ll.iterator();
+//                while (iter.hasNext()) {
+//                    JSONObject coord = new JSONObject();
+//                    LatLng point = iter.next();
+//                    coord.put(LAT, point.latitude);
+//                    coord.put(LONG, point.longitude);
+//                    locationJsonArray.put(coord);
+//                }
+//
+//            }
+//            j.put(FIELD_LOC_LIST, locationJsonArray);
+            jsonArray.put(j);
+
+        }
+
+        return jsonArray;
+    }
+
+}
--- MyParkinsons/app/src/main/java/edu/dartmouth/cs/myparkinsons/MainActivity.java
+++ MyParkinsons/app/src/main/java/edu/dartmouth/cs/myparkinsons/MainActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.content.ComponentName;
 import android.content.Context;
@@ -13,6 +17,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.ServiceConnection;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -38,6 +43,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -46,8 +56,16 @@ import java.util.List;
 import java.util.Random;
 
 
+// TODO: import google play libraries, set up with API console
+
 public class MainActivity extends FragmentActivity implements ServiceConnection {
 
+
+    public static final String PROPERTY_REG_ID = "reg_id_field";
+    public static final String PROPERTY_APP_VERSION = "app_version_field";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    private static final String TAG = "MyP Main";
 
     private Messenger serviceMessenger = null;
     boolean isBound;
@@ -55,10 +73,41 @@ public class MainActivity extends FragmentActivity implements ServiceConnection 
             new IncomingMessageHandler());
 
     private ServiceConnection connection = this;
+    private DataSource dataSource;
 
     //http://www.learn-android-easily.com/2013/06/android-viewflipper-example.html
     private float lastX;
     private float lastY;
+
+    // references for google play
+    private Context context;
+    private GoogleCloudMessaging gcm;
+    public String regId;
+
+    /**
+     * Substitute you own sender ID here. This is the project number you got
+     * from the API Console.
+     */
+    private String SENDER_ID = "THISISAFAKENUMBER";
+
+    private IntentFilter mMessageIntentFilter;
+    private BroadcastReceiver mMessageUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle extras = intent.getExtras();
+            String msg = extras.getString("message");
+//            long id = Long.getLong(msg);
+            Log.d("MainActivity", "got request " + msg);
+
+            // TODO: display prompt notification when doctor sends ping
+            if (msg != null) {
+                Log.d("MainActivity", "Received message: " + msg);
+//                dataSource.open();
+//                dataSource.deleteEntry(msg);
+//                dataSource.close();
+            }
+        }
+    };
 
 
     private ListView listView;
@@ -69,14 +118,34 @@ public class MainActivity extends FragmentActivity implements ServiceConnection 
         setContentView(R.layout.activity_main);
 
         // set up periodic notification requests
-        SharedPreferences prefs = getSharedPreferences(SettingsActivity.SHARED_PREFERENCES_KEY, MODE_PRIVATE);
+//        SharedPreferences prefs = getSharedPreferences(SettingsActivity.SHARED_PREFERENCES_KEY, MODE_PRIVATE);
 
+        // set up intent filter
+        mMessageIntentFilter = new IntentFilter();
+        mMessageIntentFilter.addAction("GCM_NOTIFY");
+
+        // set up references
+        context = getApplicationContext();
+
+        // check device for Play Services APK
+        String services = Boolean.toString(checkPlayServices());
+        Log.d(TAG, "Play services? " + services);
+        if (checkPlayServices()) {
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regId = getRegistrationId(context);
+            if (regId.isEmpty()) {
+                registerInBackground();
+            }
+            Log.d(TAG, "regId: " + regId);
+        }
+
+        // set up service
         startService(new Intent(MainActivity.this, TrackingService.class));
         doBindService();
 
         ExerciseItem[] items = new ExerciseItem[10];
 
-        DataSource dataSource = new DataSource(this);
+        dataSource = new DataSource(this);
         dataSource.open();
 
         for (int i = 0; i < 10; i++) {
@@ -108,11 +177,7 @@ public class MainActivity extends FragmentActivity implements ServiceConnection 
         final ExerciseLogArrayAdapter adapter = new ExerciseLogArrayAdapter(this, R.layout.exercise_log_row, list);
 
         listView = (ListView) findViewById(R.id.card_listView);
-
         listView.setAdapter(adapter);
-
-
-
         listView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -172,6 +237,46 @@ public class MainActivity extends FragmentActivity implements ServiceConnection 
                 return false;
             }
         });
+
+    }
+
+    private void uploadData() {
+
+        dataSource.open();
+        List<ExerciseItem> entryList = dataSource.fetchItems();
+        dataSource.close();
+
+        AsyncTask uploadTask = new AsyncTask<List, Void, Void>() {
+            @Override
+            protected Void doInBackground(List... params) {
+
+                List<ExerciseItem> entryList = (List<ExerciseItem>) params[0];
+                HistoryUploader.updateHistory(context, entryList, regId);
+
+                Void v = null;
+                return v;
+            }
+        };
+
+        uploadTask.execute();
+
+//        AsyncTask uploadTask = new AsyncTask<Object, Void, Void>() {
+//            @Override
+//            protected Void doInBackground(Object... params) {
+//                ArrayList<ExerciseEntry> entryList = (ArrayList<ExerciseEntry>) params[0];
+//                Void hello;
+//                hello = null;
+//                try {
+//                    historyUploader.updateHistory(getActivity(), entryList, regId);
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//
+//                }
+//                return hello;
+//            }
+//        };
+//
+//        uploadTask.execute((Object) entryList);
 
     }
 
@@ -247,13 +352,21 @@ public class MainActivity extends FragmentActivity implements ServiceConnection 
     @Override
     protected void onResume() {
         listView.invalidateViews();
+        registerReceiver(mMessageUpdateReceiver, mMessageIntentFilter);
         super.onResume();
+        checkPlayServices();
     }
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
 
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(mMessageUpdateReceiver);
+        super.onPause();
     }
 
     @Override
@@ -341,6 +454,151 @@ public class MainActivity extends FragmentActivity implements ServiceConnection 
 
 
     }
+
+    ////////// TAKEN FROM POSTACTIVITY FROM LECTURE NOTES: ADAPTED BY TDC ON 2/23
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If it
+     * doesn't, display a dialog that allows users to download the APK from the
+     * Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Gets the current registration ID for application on GCM service.
+     * <p>
+     * If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing
+     *         registration ID.
+     */
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+//        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION,
+//                Integer.MIN_VALUE);
+//        int currentVersion = getAppVersion(context);
+//        if (registeredVersion != currentVersion) {
+//            return "";
+//        }
+        return registrationId;
+    }
+
+    /**
+     * @return Application's {@code SharedPreferences}.
+     */
+    private SharedPreferences getGCMPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences,
+        // but
+        // how you store the regID in your app is up to you.
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * <p>
+     * Stores the registration ID and app versionCode in the application's
+     * shared preferences.
+     */
+    private void registerInBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        Log.d("Registration", "attempting to get google cloud messaging instance");
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                        Log.d("Registration", "got new GCM instance");
+                    }
+                    regId = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regId;
+
+                    // You should send the registration ID to your server over
+                    // HTTP,
+                    // so it can use GCM/HTTP or CCS to send messages to your
+                    // app.
+                    // The request to your server should be authenticated if
+                    // your app
+                    // is using accounts.
+                    ServerUtilities.sendRegistrationIdToBackend(context, regId);
+
+                    // For this demo: we don't need to send it because the
+                    // device
+                    // will send upstream messages to a server that echo back
+                    // the
+                    // message using the 'from' address in the message.
+
+                    // Persist the regID - no need to register again.
+                    storeRegistrationId(context, regId);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                Log.i(TAG, "gcm register msg: " + msg);
+            }
+        }.execute(null, null, null);
+    }
+
+    /**
+     * Stores the registration ID and app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context
+     *            application's context.
+     * @param regId
+     *            registration ID
+     */
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+
 
 }
 
